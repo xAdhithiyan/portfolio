a994d668c2d89a8bb0222bcb9107795294a53358
adhithiyan <adhithiyan.work@gmail.com>
2024-01-25 20:54:43 +0530

    add: layout and styling for home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
+    fontFamily: {
+      custom: ['VT323', 'monospace'],
+    },
+    backgroundColor: {
+      dracula: '#282a36',
+    },
   },
   plugins: [],
 };
